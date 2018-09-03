ce874987972f2747febe2b4ef6d197ef26c05ee0
Андрій Копитко <yovjik89@gmail.com>
2018-09-03 23:59:08 +0300

    Add project files.

---
+++ HW4/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW4
+{
+    class Person
+    {
+        //A two private fields
+        private string _name;
+        private int _birthYear;
+        //B two properties for access
+        public string Name 
+        {
+            get
+            {
+                return _name;
+            }
+        }
+        public int BirthYear
+        {
+            get
+            {
+                return _birthYear;
+            }
+        }
+        //C default constructor and constructor with 2 parameters 
+        public Person() {}
+        public Person(string name, int birthYear)
+        {
+            _name = name;
+            _birthYear = birthYear;
+        }
+        //D methods
+        //calculate the age 
+        public int Age()
+        {
+            int age = DateTime.Now.Year - _birthYear;
+            return age;
+        }
+        //input information about person
+        public Person Input()
+        {
+            string name;
+            int birthYear;
+            Console.Write("Name: ");
+            name = Console.ReadLine();
+            Console.Write("The birthday year: ");
+            while (!Int32.TryParse(Console.ReadLine(), out birthYear) || (birthYear > DateTime.Now.Year) 
+                || (birthYear <= (DateTime.Now.Year - 120)))
+            {
+                Console.WriteLine("You have entered invalid value for 'year'. Check please and try again.");
+                Console.Write("\nThe birthday year: ");
+            }
+            return new Person(name, birthYear);
+        }
+        //change the name of person
+        public string ChangeName()
+        {
+            this._name = "Very Young";
+            return this._name;
+        }
+        //output information about person
+        public override string ToString()
+        {
+            return "Name - " + _name + "\nThe birthday year - " + _birthYear.ToString();
+        }
+        public void Output()
+        {
+            Console.WriteLine(ToString());
+        }
+        //Persons with the same names
+        public static bool operator == (Person first, Person second)
+        {
+            return first._name == second._name;
+        }
+        public static bool operator !=(Person first, Person second)
+        {
+            return !(first == second);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //An array of data for six people
+            Person[] persons = new Person[6];        
+            Console.WriteLine("\n\tPlease enter information about each person.");
+            for (int i = 0; i < 6; i++)
+            {
+                Person p = new Person();
+                persons[i] = p;
+                Console.WriteLine("\n Person #{0}", (i + 1));
+                persons[i] = persons[i].Input();
+            }
+            //Output information about the name and age of each person
+            Console.WriteLine("\n\tInformation about all persons:\n");
+            foreach (Person person in persons)
+            {
+                int age = person.Age();
+                Console.WriteLine("Name - {0},\tAge - {1} years.", person.Name, age);
+            }
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadKey();
+            //Output information about person with changed name (if age<16)
+            Console.WriteLine("\n\tInformation about each person:");
+            foreach (Person person in persons)
+            {
+                int x = person.Age();
+                if (x < 16)
+                {
+                    person.ChangeName();
+                }
+                person.Output();
+            }
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadKey();
+            //Output information about persons with the same names 
+            Console.WriteLine("\n\tPersons with the same names:");
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = i + 1; j < 6; j++)
+                {
+                    if (persons[i] == persons[j])
+                    {
+                        Console.Write("Person #{0}\n", (i + 1));
+                        persons[i].Output();
+                        Console.Write("Person #{0}\n", (j + 1));
+                        persons[j].Output();
+                        Console.WriteLine();
+                    }
+                }
+            }            
+            Console.WriteLine("\nPress any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
